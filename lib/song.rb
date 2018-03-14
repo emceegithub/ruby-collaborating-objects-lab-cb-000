5aec1f39f01d638a36a003376f1d821d495f82a7
Mark Cruz <markchriscruz@yahoo.com>
2018-03-14 23:29:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,19 @@
 class Song
-  attr_accessor :name, :artist
-
-  @@all = []
+  attr_accessor :artist, :name
 
   def initialize(name)
     @name = name
   end
-  
-  def self.new_by_filename(filename)
-    temp_array = []
-    temp_array = filename.split(" - ")
-    artist_name = temp_array[0]
-    song_name = temp_array[1]
-    song = Song.new(song_name)
-    song.artist_name = artist_name
-    return song
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
-  
+
+  def self.new_by_filename(file)
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    song.artist_name = song_info[0]
+    song
+  end
 end
