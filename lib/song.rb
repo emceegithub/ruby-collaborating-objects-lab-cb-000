39de3d01672c5049a065f48b19981eabe52e9e27
Mark Cruz <markchriscruz@yahoo.com>
2018-03-14 23:09:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,10 +12,13 @@ class Song
     temp_array = filename.split(" - ")
     artist = temp_array[0]
     song = temp_array[1]
+    new_artist = Artist.new(artist)
+    new_artist.add_song(song)
+    new_artist.save
     # artist, song = filename.split(" - ")
-    new_song = self.new(song)
-    new_song.artist_name = artist
-    new_song.save
+    # new_song = self.new(song)
+    # new_song.artist_name = artist
+    # new_song.save
   end
   
 end
