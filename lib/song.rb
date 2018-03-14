e027c8e85115c90ab95281d716786540574d6f0c
Mark Cruz <markchriscruz@yahoo.com>
2018-03-14 22:54:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,9 +8,14 @@ class Song
   end
   
   def self.new_by_filename(filename)
-    artist, song = filename.split(" - ")
+    temp_array = []
+    temp_array = filename.split(" - ")
+    artist = temp_array[0]
+    song = temp_array[1]
+    # artist, song = filename.split(" - ")
     new_song = self.new(song)
     new_song.artist_name = artist
     new_song.save
   end
+  
 end
